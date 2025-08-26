9663353d41f18f6454b2eef356a6f7edabcd2e0c
Mitchell Williams <168118315+MitchellWilliams8@users.noreply.github.com>
2025-08-26 16:08:15 -0400

    Adds a projectile that can take out one type of enemy

--- FrogRunner.py
+++ FrogRunner.py
@@ -1,5 +1,6 @@
 import simpleGE, pygame, random
 
+# Game scene
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
@@ -13,7 +14,10 @@ class Game(simpleGE.Scene):
         self.platform = Platform(self)
         self.goldScarab = goldScarab(self)
         self.beetle = Beetle(self)
-        self.sprites = [self.frog, self.ground, self.fly, self.lblScore, self.platform, self.goldScarab, self.beetle]
+        self.projectile = Projectile(self)
+        self.sprites = [self.frog, self.ground, self.fly,
+                        self.lblScore, self.platform, self.goldScarab,
+                        self.beetle, self.projectile]
 
     def process(self):
         if self.frog.collidesWith(self.fly):
@@ -31,6 +35,14 @@ class Game(simpleGE.Scene):
             self.score -= 2
             self.lblScore.text = f"Score: {self.score}"
 
+        if self.projectile.collidesWith(self.fly):
+            self.fly.reset()
+            self.projectile.reset()
+
+        if self.projectile.collidesWith(self.beetle):
+            self.projectile.reset()
+
+# Player sprite
 class Frog(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -52,6 +64,8 @@ class Frog(simpleGE.Sprite):
             if not self.inAir:
                 self.addForce(20, 90)
                 self.inAir = True
+        if self.scene.isKeyPressed(pygame.K_s):
+            self.scene.projectile.shoot()
 
         self.inAir = True
         if self.collidesWith(self.scene.ground):
@@ -66,6 +80,7 @@ class Frog(simpleGE.Sprite):
                 self.dy = 0
                 self.inAir = False
 
+# Ground
 class groundBarrier(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -73,6 +88,7 @@ class groundBarrier(simpleGE.Sprite):
         self.setSize(1400, 50)
         self.position = (50, 450)
 
+# Moving platform
 class Platform(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -84,6 +100,7 @@ class Platform(simpleGE.Sprite):
         self.dx = random.randint(-8, -4)
         self.dy = random.randint(0, 0)
 
+# Enemy 1
 class Fly(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -92,11 +109,13 @@ class Fly(simpleGE.Sprite):
         self.reset()
         #self.position = (500, 400)
     def reset(self):
-        self.x = 700
+        self.x = 625
         self.y = 275
         self.dx = random.randint(-8,-4)
         self.dy = random.randint(0, 0)
 
+
+# Enemy 2
 class Beetle(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -105,11 +124,12 @@ class Beetle(simpleGE.Sprite):
         self.reset()
 
     def reset(self):
-        self.x = 600
+        self.x = 625
         self.y = 400
         self.dx = random.randint(-8,-4)
         self.dy = random.randint(0, 0)
 
+# Scoring object
 class goldScarab(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
@@ -117,17 +137,34 @@ class goldScarab(simpleGE.Sprite):
         self.setSize(30, 30)
         self.reset()
     def reset(self):
-        self.x = 700
+        self.x = 625
         self.y = 200
         self.dx = random.randint(-8,-4)
         self.dy = random.randint(0, 0)
 
+# Projectile
+class Projectile(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Projectile.png")
+        self.setSize(50, 50)
+        self.setBoundAction(self.reset())
+
+    def shoot(self):
+        self.position = (self.scene.frog.x, self.scene.frog.y)
+        self.speed = 7
+
+    def reset(self):
+        self.position = (-100, -100)
+        self.speed = 0
+
+# Score
 class LblScore(simpleGE.Label):
     def __init__(self):
         super().__init__()
         self.clearBack = True
         self.text = "Score: 0"
-        self.center = (100,100)
+        self.center = (60,25)
 
 def main():
     game = Game()
