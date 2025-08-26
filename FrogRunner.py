6410834ed98f692fcacff89d22e9f8dc2d65375f
Mitchell Williams <168118315+MitchellWilliams8@users.noreply.github.com>
2025-08-26 13:15:22 -0400

    Adds movement and collision with the fly along with a score label

--- FrogRunner.py
+++ FrogRunner.py
@@ -1,13 +1,21 @@
-import simpleGE, pygame
+import simpleGE, pygame, random
 
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("Black.png")
+        self.lblScore = LblScore()
+        self.score = 0
         self.frog = Frog(self)
         self.ground = groundBarrier(self)
         self.fly = Fly(self)
-        self.sprites = [self.frog, self.ground, self.fly]
+        self.sprites = [self.frog, self.ground, self.fly, self.lblScore]
+
+    def process(self):
+        if self.frog.collidesWith(self.fly):
+            self.fly.reset()
+            self.score -= 1
+            self.lblScore.text = f"Score: {self.score}"
 
 class Frog(simpleGE.Sprite):
     def __init__(self, scene):
@@ -28,7 +36,7 @@ class Frog(simpleGE.Sprite):
 
         if self.scene.isKeyPressed(pygame.K_SPACE):
             if not self.inAir:
-                self.addForce(15, 90)
+                self.addForce(20, 90)
                 self.inAir = True
 
         self.inAir = True
@@ -50,7 +58,20 @@ class Fly(simpleGE.Sprite):
         super().__init__(scene)
         self.setImage("Fly.png")
         self.setSize(30, 30)
-        self.position = (500, 400)
+        self.reset()
+        #self.position = (500, 400)
+    def reset(self):
+        self.x = 700
+        self.y = 400
+        self.dx = random.randint(-8,-4)
+        self.dy = random.randint(0, 0)
+
+class LblScore(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.clearBack = True
+        self.text = "Score: 0"
+        self.center = (100,100)
 
 def main():
     game = Game()
