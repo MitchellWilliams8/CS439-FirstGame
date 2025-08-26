7de1f6702544c62fb9209b0230b8587ac1707183
Mitchell Williams <168118315+MitchellWilliams8@users.noreply.github.com>
2025-08-25 21:45:53 -0400

    Fixes jumping physics to be more realistic

--- FrogRunner.py
+++ FrogRunner.py
@@ -5,9 +5,8 @@ class Game(simpleGE.Scene):
         super().__init__()
         self.setImage("Black.png")
         self.frog = Frog(self)
-        self.barrier = Barrier(self)
-        self.sprites = [self.frog, self.barrier]
-
+        self.ground = groundBarrier(self)
+        self.sprites = [self.frog, self.ground]
 
 class Frog(simpleGE.Sprite):
     def __init__(self, scene):
@@ -15,23 +14,33 @@ class Frog(simpleGE.Sprite):
         self.setImage("Frog.png")
         self.setSize(50, 50)
         self.position = (50, 400)
+        self.inAir = True
 
     def process(self):
-        self.addForce(1, 270)
-        if self.collidesWith(self.scene.barrier):
-            self.dx=0
-            self.dy=0
-        if self.scene.isKeyPressed(pygame.K_SPACE):
+        if self.inAir:
+            self.addForce(1, 270)
+
+        if self.y > 500:
+            self.inAir =False
+            self.y = 500
             self.dy = 0
-            self.addForce(5, 90)
 
+        if self.scene.isKeyPressed(pygame.K_SPACE):
+            if not self.inAir:
+                self.addForce(15, 90)
+                self.inAir = True
 
+        self.inAir = True
+        if self.collidesWith(self.scene.ground):
+            if self.dy > 0:
+                self.bottom = self.scene.ground.top
+                self.dy = 0
+                self.inAir = False
 
-
-class Barrier(simpleGE.Sprite):
+class groundBarrier(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("Barrier.png")
+        self.setImage("Ground.png")
         self.setSize(1400, 50)
         self.position = (50, 450)
 
