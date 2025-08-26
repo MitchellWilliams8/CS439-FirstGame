4cb0527f9415dc45f331ef46256dd827ae270b53
Mitchell Williams <168118315+MitchellWilliams8@users.noreply.github.com>
2025-08-25 20:40:31 -0400

    Adds the ability to jump and a barrier for the ground

--- FrogRunner.py
+++ FrogRunner.py
@@ -5,7 +5,8 @@ class Game(simpleGE.Scene):
         super().__init__()
         self.setImage("Black.png")
         self.frog = Frog(self)
-        self.sprites = [self.frog]
+        self.barrier = Barrier(self)
+        self.sprites = [self.frog, self.barrier]
 
 
 class Frog(simpleGE.Sprite):
@@ -13,9 +14,26 @@ class Frog(simpleGE.Sprite):
         super().__init__(scene)
         self.setImage("Frog.png")
         self.setSize(50, 50)
+        self.position = (50, 400)
+
+    def process(self):
+        self.addForce(1, 270)
+        if self.collidesWith(self.scene.barrier):
+            self.dx=0
+            self.dy=0
+        if self.scene.isKeyPressed(pygame.K_SPACE):
+            self.dy = 0
+            self.addForce(5, 90)
+
+
+
+
+class Barrier(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Barrier.png")
+        self.setSize(1400, 50)
         self.position = (50, 450)
-
-
 
 def main():
     game = Game()
